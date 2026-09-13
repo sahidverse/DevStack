fde904945185cbd705995139cb44cfb5bac22963
sahidverse <mdsahidhasan2020@gmail.com>
2026-09-13 11:46:17 +0600

    nav created

---
+++ src/component/Nav.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../assets/logo-text.png'
+const Nav = () => {
+    return (
+        <div className=' sticky top-0 flex justify-between container mx-auto py-2'>
+             <img src={logo} className='h-[32] w-[136] ' alt="logo img" />
+
+            <ul className='flex gap-3 text-xs my-auto '>
+                <li className='text-[#DB2777]'>Home</li>
+                <li>Technologies</li>
+                <li>Project</li>
+                <li>About</li>
+                <li>contact</li>
+            </ul>
+            <div className='gap-2'>
+            <button className='h-[92] w-[39] bold py-1 px-2 rounded-3xl '>Sign In</button>
+            <button className='h-[92] w-[39] bg-[#D91B7E] text-white bold py-1 px-2 rounded-3xl '>Sign Up</button>
+            </div>
+
+        </div>
+
+        
+    );
+};
+
+export default Nav;
