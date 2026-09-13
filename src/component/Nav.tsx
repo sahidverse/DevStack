f9e2164d1d34fcb45e59c0423d748a7421482cbf
sahidverse <mdsahidhasan2020@gmail.com>
2026-09-13 21:53:02 +0600

    deploy problem fixed

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -2,24 +2,22 @@
 import logo from '../assets/logo-text.png'
 const Nav = () => {
     return (
-        <div className='bg-white sticky top-0 flex justify-between container mx-auto py-1'>
-             <img src={logo} className='h-[32] w-[136] ' alt="logo img" />
+        <div className='bg-white sticky top-0 flex items-center justify-between container mx-auto py-2'>
+            <img src={logo} className='h-8 w-auto' alt="logo img" />
 
-            <ul className='flex gap-3 text-xs my-auto '>
+            <ul className='flex items-center gap-3 text-xs'>
                 <li className='text-[#DB2777]'>Home</li>
                 <li>Technologies</li>
                 <li>Project</li>
                 <li>About</li>
                 <li>contact</li>
             </ul>
-            <div className='gap-2'>
-            <button className='h-[92] w-[39] bold py-1 px-2 rounded-3xl '>Sign In</button>
-            <button className='h-[92] w-[39] bg-[#D91B7E] text-white bold py-1 px-2 rounded-3xl '>Sign Up</button>
+
+            <div className='flex items-center gap-2'>
+                <button className='rounded-full border border-[#DB2777] px-4 py-2 text-sm font-semibold text-[#DB2777]'>Sign In</button>
+                <button className='rounded-full bg-[#D91B7E] px-4 py-2 text-sm font-semibold text-white'>Sign Up</button>
             </div>
-
         </div>
-
-        
     );
 };
 
